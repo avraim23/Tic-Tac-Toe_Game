e0261eccfbece79be11fc8aa50401b8e1d614518
Your Nameavraim23 <avraim23>
2023-09-25 14:53:17 +0300

    Tic-Tac-Toe Game

---
+++ __astcache/d@@workspace@private@gui projects@tic-tac-toe game project/unit1.cpp
@@ -0,0 +1,101 @@
+﻿//---------------------------------------------------------------------------
+
+#include <fmx.h>
+#pragma hdrstop
+
+#include "Unit1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.fmx"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+char CurrectPlayerSymmbol = 'X' ;
+void TForm1::disable()
+{
+	Button1->Enabled = false ;
+	Button2->Enabled = false ;
+	Button3->Enabled = false ;
+	Button4->Enabled = false ;
+	Button5->Enabled = false ;
+	Button6->Enabled = false ;
+	Button7->Enabled = false ;
+	Button8->Enabled = false ;
+	Button9->Enabled = false ;
+}
+bool TForm1::checkIsWinner ()
+{
+	if ( !Button1->Text.IsEmpty() && Button1->Text == Button2->Text && Button1->Text == Button3->Text )
+	{
+			return true ;
+	}
+	else if ( !Button4->Text.IsEmpty() && Button4->Text == Button5->Text && Button4->Text == Button6->Text )
+	{
+			return true ;
+	}
+	else if ( !Button7->Text.IsEmpty() && Button7->Text == Button8->Text && Button7->Text == Button9->Text )
+	{
+			return true ;
+	}
+	else if ( !Button1->Text.IsEmpty() && Button1->Text == Button4->Text && Button1->Text == Button7->Text )
+	{
+			return true ;
+	}
+	else if ( !Button2->Text.IsEmpty() && Button2->Text == Button5->Text && Button2->Text == Button8->Text )
+	{
+			return true ;
+	}
+	else if ( !Button3->Text.IsEmpty() && Button3->Text == Button6->Text && Button3->Text == Button9->Text )
+	{
+			return true ;
+	}
+	else if ( !Button1->Text.IsEmpty() && Button1->Text == Button5->Text && Button1->Text == Button9->Text )
+	{
+			return true ;
+	}
+	else if ( !Button3->Text.IsEmpty() && Button3->Text == Button5->Text && Button3->Text == Button7->Text )
+	{
+			return true ;
+	}
+	else
+	{
+		return false ;
+	}
+}
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+	CurrentPlayerSymbolLabel->Text =  CurrectPlayerSymmbol ;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::ButtonClick(TObject *Sender)
+{
+	TButton* CLickedBuitton = dynamic_cast<TButton*>(Sender) ;
+	CLickedBuitton->Text = CurrectPlayerSymmbol ;
+	CLickedBuitton->Enabled = false ;
+
+
+
+
+
+	if (checkIsWinner())
+	{
+		WinnerLabel->Text = "YOU WON !!" ;
+		disable() ;
+		return ;
+	}
+
+	if ( CurrectPlayerSymmbol=='X' )
+	{
+		CurrectPlayerSymmbol = 'O' ;
+	}
+	else
+	{
+		CurrectPlayerSymmbol = 'X' ;
+	}
+	CurrentPlayerSymbolLabel->Text =  CurrectPlayerSymmbol ;
+}
+//---------------------------------------------------------------------------
+
+
---
+++ __astcache/d@@workspace@private@gui projects@tic-tac-toe game project/unit1.h
@@ -0,0 +1,38 @@
+﻿//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TLabel *Label1;
+	TLabel *CurrentPlayerSymbolLabel;
+	TLabel *WinnerLabel;
+	TButton *Button1;
+	TButton *Button2;
+	TButton *Button3;
+	TButton *Button4;
+	TButton *Button5;
+	TButton *Button6;
+	TButton *Button7;
+	TButton *Button8;
+	TButton *Button9;
+	void __fastcall ButtonClick(TObject *Sender);
+	private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+	bool checkIsWinner () ;
+    void disable() ;
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
